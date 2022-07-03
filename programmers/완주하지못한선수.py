95bf5ea51cb0d7d2f3805fd146a4bc485493837a
KIMHYOJUN <olzitoon@naver.com>
2022-07-03 19:25:11 +0900

    전화번호 목록

--- programmers/완주하지못한선수.py
+++ programmers/완주하지못한선수.py
@@ -1,13 +1,21 @@
-def solution(participant,completion):
-    participant.sort()
-    completion.sort()
+# def solution(participant,completion):
+#     participant.sort()
+#     completion.sort()
 
-    answer = " "
-    for i in range(len(participant)-1):
-        if participant[i] != completion[i]:
-            return participant[i]
+#     answer = " "
+#     for i in range(len(participant)-1):
+#         if participant[i] != completion[i]:
+#             return participant[i]
     
-    answer = participant[-1]
-    return answer
+#     answer = participant[-1]
+#     return answer
+
+
+import collections
+
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[-1]
 
 print(solution(["leo", "kiki", "eden"],["eden", "kiki"]))
