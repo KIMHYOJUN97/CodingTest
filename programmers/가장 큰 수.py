73adcbf392b43933360bb752aedd4dc7d0793a5d
KIMHYOJUN <olzitoon@naver.com>
2022-07-20 01:00:43 +0900

    hard sort

---
+++ programmers/가장 큰 수.py
@@ -0,0 +1,75 @@
+#정렬
+#LV2
+
+#순열 시간초과
+# from itertools import permutations
+
+# def solution(numbers):
+#     answer = ''
+#     numbers = list(map(str,numbers))
+#     per_list = list(permutations(numbers))
+#     answer_list = []
+#     for per in per_list:
+#         tmp = ''
+#         for p in per:
+#             tmp += p
+#         answer_list.append(tmp)
+    
+#     answer_list=list(map(int,answer_list))
+#     answer = str(max(answer_list))
+
+#     return answer
+
+#3, 304  3304 3043  320 32 32320 32032  37 371  37371 37137
+#어짜피 자리수는 0-9이니까 9개의 index 생성 후 큰 자리수대로 정렬?
+#x[0] x[-1]>=x[0] int(x)
+#값이 같으면 짧은쪽이 먼저=>전처리 해줘야함
+def solution(numbers):
+    answer = ''
+    numbers = list(map(str,numbers))
+    numbers.sort(key=lambda x:-int(x[0]))
+    # answer_list = [['1' for i in range(5)]] * len(numbers)
+    answer_list = [['1' for i in range(5)] for _ in range(len(numbers))] 
+    i  = 0
+    for num in numbers:
+        j = 0
+        for n in num:
+            answer_list[i][j] = n
+            j+=1
+        answer_list[i][4] = len(num)
+        i += 1
+    #answer_list.sort(key=lambda x:(map(int,(x[0]),x[1],x[2],x[3])))
+    answer_list.sort(key=lambda x:(-int(x[0]),int(x[1]),int(x[2]),int(x[3])))
+
+    for ans in answer_list:
+        for i in range(ans[4]):
+            answer += ans[i]
+    # while numbers:
+    #     max = 0
+    #     max_all = ''
+    #     index = 0
+    #     for i,num in enumerate(numbers):
+    #         if int(num[0]) > max:
+    #             max = int(num[0])
+    #             max_all = num
+    #             index = i
+    #     answer_list.append([max_all,len(max_all)])
+    #     numbers.pop(index)
+    #len값 조사 중 x[0]값이 x[1]값보다 작은경우
+    # answer_list.sort(key=lambda x:(x[1],-int(x[0])))
+
+    # for ans in answer_list:
+    #     answer += ans[0]
+    answer = int(answer)
+    return str(answer)
+
+print(solution([101, 10, 232, 23]))
+
+def solution(numbers):
+    answer = ''
+    numbers=map(str,numbers)
+    numbers.sort(key=lambda x:x*3,reversed=True)
+    for num in numbers:
+        answer += num
+    return str(int(answer))
+# 2323210110
--- programmers/섬 연결하기.py
+++ programmers/섬 연결하기.py
@@ -9,25 +9,39 @@
 def solution(n, costs):
     answer = 0
     visited = [0] * n
+    vertex = []
     costs.sort(key=lambda x:(x[2],x[0],x[1]))
     for cost in costs:
-        count = 0
         if visited[cost[0]]==0 and visited[cost[1]]==0:
             answer += cost[2]
             visited[cost[0]]=1
             visited[cost[1]]=1
+            vertex.append([cost[0],cost[1]])
         elif visited[cost[0]]==1 and visited[cost[1]]==0:
             answer += cost[2]
             visited[cost[1]]=1
+            vertex.append([cost[0],cost[1]])
         else:
             continue
-        for v in visited:
-            if v == 0:
-                count +=1
-        if count ==0:
-            print(costs)
-            print(cost)
-            return answer
+    
+    return answer
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2]) # 비용기준으로 오름차순 정렬
+    connect = set([costs[0][0]]) # 연결을 확인하는 집합
+    
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect:
+                continue
+            if cost[0] in connect or cost[1] in connect:
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
+                
+    return answer
 
 print(solution(5,[[0, 1, 1], [0, 4, 5], [2, 4, 1], [2, 3, 1], [3, 4, 1]]))
 
