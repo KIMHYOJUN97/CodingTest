1600488d6ddbe6cf9aa78afe0b4c6af59e257201
KIMHYOJUN <olzitoon@naver.com>
2022-07-23 17:51:32 +0900

    소수 찾기

--- programmers/소수 찾기.py
+++ programmers/소수 찾기.py
@@ -2,6 +2,9 @@
 from itertools import permutations
 
 def is_prime_num(n):
+    if(n<2):
+        return False
+    
     for i in range(2, n):
         if n % i == 0:
             return False # i로 나누어 떨어지면 소수가 아니므로 False 리턴
