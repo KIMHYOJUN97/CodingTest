b415cd63ea2b4f63d683cc045033df1a33c4819a
KIMHYOJUN <olzitoon@naver.com>
2022-08-10 17:45:06 +0900

    10866덱

---
+++ baekjoon/10866 덱.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n = int(input())
+dq = deque()
+answer = []
+for i in range(n):
+    question = list(input().split())
+    if question[0] == "push_back":
+        dq.append(question[1])
+    elif question[0] == "push_front":
+        dq.extendleft(question[1])
+    elif question[0] == "front":
+        if dq:
+            answer.append(dq[0])
+        else:
+            answer.append(-1)
+    elif question[0] == "back":
+        if dq:
+            answer.append(dq[-1])
+        else:
+            answer.append(-1)
+    elif question[0] == "pop_front":
+        if dq:
+            answer.append(dq.popleft())
+        else:
+            answer.append(-1)
+    elif question[0] == "pop_back":
+        if dq:
+            answer.append(dq.pop())
+        else:
+            answer.append(-1)
+    elif question[0] == "size":
+        answer.append(len(dq))
+    else:
+        if dq:
+            answer.append(0)
+        else:
+            answer.append(1)
+
+for i in range(len(answer)):
+    print(answer[i])
