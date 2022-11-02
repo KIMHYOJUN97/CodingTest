3acd43d6c50c5ba597a5b7b9fffd8eae734807a5
KIMHYOJUN <olzitoon@naver.com>
2022-11-02 22:59:04 +0900

    1206 문제해결

--- swexpert/1206 문제해결 기본 1일차-view.py
+++ swexpert/1206 문제해결 기본 1일차-view.py
@@ -1,13 +1,12 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-t = 1
+t = 10
 for j in range(t):
     n = int(input())
     answer = 0
     building = list(map(int,input().split()))
-    for i in range(2,n-1):
-        flag = 0
+    for i in range(2,n-2):
         if(building[i-2]>building[i] or building[i-1]>building[i] or building[i+1]>building[i] or building[i+2]>building[i]):
             continue
         height = [building[i-2],building[i-1],building[i+1],building[i+2]]
